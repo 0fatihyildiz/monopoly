e687a05b937471ed5893fda339d1f513993abe78
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2024-04-04 22:58:26 +0300

    3d

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,9 +1,23 @@
+import { Torus } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
+import { Suspense } from "react";
+
+
 function App() {
 
-
   return (
-    <>
-    </>
+    <Canvas>
+      <Suspense>
+        <Physics debug>
+          <RigidBody colliders={"hull"} restitution={2}>
+            <Torus />
+          </RigidBody>
+
+          <CuboidCollider position={[0, -2, 0]} args={[20, 0.5, 20]} />
+        </Physics>
+      </Suspense>
+    </Canvas>
   )
 }
 
